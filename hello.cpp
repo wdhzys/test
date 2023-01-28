8614d287baf854d80fa69b819c94a00193242c98
wdhzys <1324940758@qq.com>
2023-01-28 11:07:44 +0800

    1.0

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"hello world"<<std::endl;
+    std::cout<<"hello world"<<std::endl;
+    std::cout<<"hello world"<<std::endl;
+    std::cout<<"hello world"<<std::endl;
+    //wdkqowwhtfy
+
+    std::cout<<"hello world"<<std::endl;
+    system("pause");
+    return 0;
+}
